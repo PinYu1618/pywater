03bd967c082d3df3227ac0d433a0058859a8ca86
PinYu1618 <pycc1618@gmail.com>
2023-12-01 03:24:39 +0800

    qt5 works

--- pywater/__main__.py
+++ pywater/__main__.py
@@ -1,5 +1,14 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
 def main():
     print("Hello world.")
+    app = QApplication([])
+    win = QWidget()
+    win.show()
+    sys.exit(app.exec_())
 
 
 if __name__ == "__main__":
