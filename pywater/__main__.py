f60752684ed293b711837fc4170cd0428460c89e
PinYu1618 <pycc1618@gmail.com>
2023-12-01 07:55:17 +0800

    fea: tabs

--- pywater/__main__.py
+++ pywater/__main__.py
@@ -1,10 +1,75 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+import typing
+from PyQt5 import QtCore
+from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtWidgets import (
+    QApplication,
+    QWidget,
+    QPushButton,
+    QLabel,
+    QTabWidget,
+    QVBoxLayout,
+    QHBoxLayout,
+    QCalendarWidget,
+)
+
+WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 300
 
 
-class Window(QMainWindow):
+class CalenderTab(QWidget):
+    def __init__(self):
+        super().__init__()
+        hbox = QHBoxLayout()
+
+        self.calendar = QCalendarWidget()
+
+        self.label = QLabel("Hello")
+        self.label.setFont(QFont("Sanserif", 15))
+        self.label.setStyleSheet("color:red")
+
+        hbox.addWidget(self.calendar)
+        hbox.addWidget(self.label)
+
+        self.setLayout(hbox)
+
+
+class Window(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("PyWater")
+        # self.setWindowIcon(QIcon('./assets/icon.png'))
+        self.setGeometry(300, 300, WINDOW_WIDTH, WINDOW_HEIGHT)
+
+        tab1 = QWidget()
+
+        tabs = QTabWidget()
+        tabs.addTab(tab1, "Tab1")
+        tabs.addTab(CalenderTab(), "History")
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(tabs)
+
+        self.setLayout(vbox)
+
+        # self.btn = QPushButton("+100ml")
+        # self.setCentralWidget(self.btn)
+
+    def onclick(self):
+        print("Clicked!")
+
+
+class App:
+    def __init__(self, window: Window):
+        self.win = window
+        self.count = 0
+        self.connect_signals()
+
+    def connect_signals(self):
+        self.win.btn.clicked.connect(self.win.onclick)
+
+    def inc(self):
+        self.count += 1
+        self.win.onclick()
 
 
 def main():
@@ -13,6 +78,7 @@ def main():
     qt = QApplication([])
     win = Window()
     win.show()
+    # App(win)
     sys.exit(qt.exec_())
 
 
