44396fdbd31749a05ffe95ef776f5928d05d37a7
PinYu1618 <pycc1618@gmail.com>
2023-12-01 09:13:14 +0800

    calender fix

--- pywater/__main__.py
+++ pywater/__main__.py
@@ -39,14 +39,20 @@ class DrinkingWaterAnimation(QWidget):
         painter = QPainter(self)
         self.drawGlass(painter)
         self.drawWater(painter)
+        self.drawFace(painter)
 
     def drawGlass(self, painter):
         # Draw a simple glass cup
-        pen = QPen(QColor(0, 0, 0))
+        painter.setRenderHint(QPainter.Antialiasing)
+        pen = painter.pen()
+        pen.setColor(QColor(0, 0, 0))  # Black color
         pen.setWidth(2)
         painter.setPen(pen)
 
-        # painter.setBrush(Qt.NoBrush)
+        brush = painter.brush()
+        # brush.setStyle(Qt.NoBrush)
+        painter.setBrush(brush)
+
         painter.drawEllipse(150, 200, 100, 30)
         painter.drawLine(150, 215, 100, 300)
         painter.drawLine(250, 215, 300, 300)
@@ -61,10 +67,61 @@ class DrinkingWaterAnimation(QWidget):
         if waterHeight > 0:
             painter.drawRect(160, 300 - self.waterLevel, 80, self.waterLevel)
 
+        # Draw a face based on water level
+        self.drawFace(painter)
+
         # Increase the water level
         self.waterLevel += 5
         if self.waterLevel > 300:
             self.waterLevel = 0
+
+    def drawFace(self, painter):
+        # Draw a smiley face or an unhappy face based on water level
+        if self.waterLevel > 200:  # Sufficient water, draw a smiley face
+            painter.setRenderHint(QPainter.Antialiasing)
+            pen = painter.pen()
+            pen.setColor(QColor(0, 0, 0))  # Black color
+            painter.setPen(pen)
+
+            font = QFont("Arial", 16, QFont.Bold)
+            painter.setFont(font)
+            painter.drawText(185, 250, "😊")  # Smiley face emoji
+        else:  # Low water level, draw an unhappy face
+            painter.setRenderHint(QPainter.Antialiasing)
+            pen = painter.pen()
+            pen.setColor(QColor(0, 0, 0))  # Black color
+            painter.setPen(pen)
+
+            font = QFont("Arial", 16, QFont.Bold)
+            painter.setFont(font)
+            painter.drawText(185, 250, "☹️")  # Unhappy face emoji
+
+
+class CalenderTab(QWidget):
+    def __init__(self):
+        super().__init__()
+        hbox = QHBoxLayout()
+
+        self.calendar = QCalendarWidget()
+        self.calendar.selectionChanged.connect(self.calendar_date)
+
+        self.label = QLabel("Hello")
+        self.label.setFont(QFont("Sanserif", 15))
+        self.label.setStyleSheet("color:red")
+
+        hbox.addWidget(self.calendar)
+        hbox.addWidget(self.label)
+
+        self.setLayout(hbox)
+
+    # def showDate(self, date):
+    #    self.lbl.setText(date.toString())
+
+    def calendar_date(self):
+        dateselected = self.calendar.selectedDate()
+        date_in_string = str(dateselected.toPyDate())
+
+        self.label.setText("Date Is : " + date_in_string)
 
 
 class Window(QWidget):
