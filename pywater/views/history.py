3f8fa420441ca6bec713a5051c49e229c980af26
PinYu1618 <pycc1618@gmail.com>
2023-12-05 04:09:40 +0800

    rewrite history view

--- pywater/views/history.py
+++ pywater/views/history.py
@@ -1,53 +1,45 @@
 from typing import Union
 
-from PyQt5.QtSql import QSqlTableModel
-from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
     QWidget,
     QLabel,
     QCalendarWidget,
-    QVBoxLayout,
     QGridLayout,
-    QTableView,
+    QPushButton,
+    QFrame,
 )
 
 
 class HistoryView(QWidget):
-    def __init__(self, parent: Union[QWidget, None] = None):
+    def __init__(self, parent: Union[QWidget, None] = None) -> None:
         super().__init__(parent)
-        grid = QGridLayout(self)
+        # layout
+        ly = QGridLayout(self)
+        self.setLayout(ly)
 
-        lbox = QVBoxLayout()
+        # header
+        header = QLabel("History Manager")
+        header.setStyleSheet("border: 2px solid black;")
+        ly.addWidget(header, 0, 0)
+
+        # modify button
+        ly.addWidget(QPushButton("Modify"), 1, 1)
+
+        # calender
         self.calendar = QCalendarWidget(self)
         self.calendar.setMouseTracking(True)
         self.calendar.selectionChanged.connect(self.calendar_date)
-        self.calendar.setFixedSize(QSize(400, 300))
+        # self.calendar.setFixedSize(QSize(400, 400))
         self.calendar.setCursor(Qt.PointingHandCursor)
-        lbox.addWidget(self.calendar)
+        ly.addWidget(self.calendar, 2, 0)
 
-        # model
-        self.model = QSqlTableModel(self)
-        self.model.setTable("records")
-        self.model.setEditStrategy(QSqlTableModel.OnFieldChange)
-        self.model.setHeaderData(0, Qt.Horizontal, "ID")
-        self.model.setHeaderData(1, Qt.Horizontal, "Date")
-        self.model.setHeaderData(2, Qt.Horizontal, "Water")
-        self.model.setHeaderData(3, Qt.Horizontal, "Weight")
-        self.model.select()
+        # data
+        ly.addWidget(QFrame(self), 2, 1)
 
-        # view
-        rbox = QVBoxLayout()
-        self.lbl = QLabel("Unknown Date")
-        self.lst = QTableView()
-        self.lst.setModel(self.model)
-        self.lst.resizeColumnsToContents()
-        rbox.addWidget(self.lbl)
-        rbox.addWidget(self.lst)
-
-        grid.addLayout(lbox, 0, 0)
-        grid.addLayout(rbox, 0, 1)
-
-        self.setLayout(grid)
+        # save button
+        self.btn_save = QPushButton("Save", self)
+        ly.addWidget(self.btn_save, 3, 1)
 
     # def showDate(self, date):
     #    self.lbl.setText(date.toString())
